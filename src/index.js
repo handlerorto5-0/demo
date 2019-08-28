4eaac09cd96391f13c8c05c18f8a1f6dbed3d1b8
excavator <str_token@mail.ru>
2019-08-28 21:13:20 +0300

    Output Management

--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
+import printMe from './print.js';
 
 function component(){
 	const element = document.createElement('div');
-	// Lodash, now imported by this script
+	const btn = document.createElement('button');
+	
 	element.innerHTML = _.join(['Hello','Webpack!'],' ');
-	element.classList.add('hello');
 
-	// add the image to our existing div
-	const myIcon = new Image();
-	myIcon.src = Icon;
-	element.appendChild(myIcon);
+	btn.innerHTML = 'Click me and check the console';
+	btn.onclick = printMe;
 
-	console.log({Data});
-
+	element.appendChild(btn);
+	
 	return element;
 };
 
