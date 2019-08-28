a72a6705cb0e2175d9dff0bff8b97f9e5e4a9c85
excavator <str_token@mail.ru>
2019-08-28 22:18:36 +0300

    DevTool: map and devpserver

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,20 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: {
     app: './src/index.js',
     print: './src/print.js'
   },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
   plugins: [
+    // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Output Management'
+      title: 'Develjpment tols'
     })
   ],
   output: {
