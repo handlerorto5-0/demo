e495fd8cfc54fb3f56356d241921ea0c3b3b4b4d
excavator <str_token@mail.ru>
2019-08-29 18:23:02 +0300

    Entry Point
    Code Splitting

--- webpack.config.js
+++ webpack.config.js
@@ -3,19 +3,19 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: {
-    app: './src/index.js',
-    print: './src/print.js'
+    index: './src/index.js',
+    another: './src/another-module.js'
   },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      title: 'Output Management'
-    })
-  ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    }
+  }
   
 };
