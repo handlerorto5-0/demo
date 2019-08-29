62f64c63afb349cb082df7068bc729ae1c494092
excavator <str_token@mail.ru>
2019-08-29 16:24:29 +0300

    Hot Module Replacement

--- webpack.config.js
+++ webpack.config.js
@@ -5,18 +5,27 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: {
-    app: './src/index.js',
-    print: './src/print.js'
+    app: './src/index.js'
+  },
+  /* without dev-server
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },*/
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
   },
   devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist'
-  },
   plugins: [
     // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Develjpment tols'
+      title: 'Hot Module Replacement'
     })
   ],
   output: {
